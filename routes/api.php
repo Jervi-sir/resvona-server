f1f39e9e0757b578232b1ccefe4ec6713a7e032b
Jervi <Jervi>
2021-10-15 11:35:41 +0100

    awake controller

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ServerController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
